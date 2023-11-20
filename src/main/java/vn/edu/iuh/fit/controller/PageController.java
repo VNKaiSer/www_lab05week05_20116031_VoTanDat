c5111830b45bed1aa6319ff5b61335bf9285fd9c
VNKaiser <tandatvok16@gmail.com>
2023-11-20 19:47:27 +0700

    redirect page

--- src/main/java/vn/edu/iuh/fit/controller/PageController.java
+++ src/main/java/vn/edu/iuh/fit/controller/PageController.java
@@ -1,4 +1,28 @@
 package vn.edu.iuh.fit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class PageController {
+    @PostMapping("/web")
+    public String redirectPage(@RequestParam(name = "role", defaultValue = "candidate") String role){
+        String page = String.format("redirect:/%s",role);
+        return page;
+    }
+
+    @GetMapping("/candidate")
+    public  String getCandidatePage(){
+        return "candidate/index";
+    }
+
+    @GetMapping("/company")
+    public  String getCompanyPage(){
+        return "company/index";
+    }
+
+
 }
